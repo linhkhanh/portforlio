5f6a9307475d24f22a9b5ce675bb3fe79abac79c
linhle <linhle@Hieus-MacBook-Air.local>
2020-09-08 12:42:12 +0800

    Updates

---
+++ precache-manifest.c5d2fb5cc628b0291e0e92b149b4708f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844416e51ac568e1a796c81ec24b24f6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "def67963216f215b00cb",
+    "url": "/portfolio/static/css/main.bdb60ad3.chunk.css"
+  },
+  {
+    "revision": "c23e68d7871647c1c4d8",
+    "url": "/portfolio/static/js/2.dccc6853.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.dccc6853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "def67963216f215b00cb",
+    "url": "/portfolio/static/js/main.49d0dfd5.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  }
+]);
